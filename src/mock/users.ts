aa65b9ee529167bb1551b7ae90962da5c2f373ac
Jarzt <dev.luanpham@gmail.com>
2024-03-10 02:27:16 +0700

    feat: add structure usecase

---
+++ src/mock/users.ts
@@ -0,0 +1,26 @@
+export const users = [
+    {
+      id: "65ecaeaf227fd89b47f9b8a7",
+      name: "Salinas Mosley",
+    },
+    {
+      id: "65ecaeafd740a5156bc708a7",
+      name: "Elnora Carpenter",
+    },
+    {
+      id: "65ecaeaf28831335c6afc65b",
+      name: "Valerie Simpson",
+    },
+    {
+      id: "65ecaeafd3b3b4b58884028c",
+      name: "Levine Trevino",
+    },
+    {
+      id: "65ecaeaf333d356bda94e6bc",
+      name: "Clark Whitney",
+    },
+    {
+      id: "65ecaeaf52617de98747bb64",
+      name: "Yolanda Fry",
+    },
+];
